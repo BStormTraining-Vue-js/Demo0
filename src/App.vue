7c8f5c23d1212983d77411417862da6e7b5d55d8
Khun <lykhun@gmail.com>
2025-05-09 09:58:06 +0200

    options api

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    },
+    methods: {
+        increase() {
+            this.count++;
+        }
+    }
+}
+</script>
+
+<template>
+    <p>{{ count }}</p>
+    <button @click="increase">Click Me</button>
+</template>
+
+<style scoped>
+p {
+    font-weight: bold;   
+}
+</style>
